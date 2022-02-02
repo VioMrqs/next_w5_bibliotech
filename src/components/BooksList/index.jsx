9f9ffb62ce9658756aa07ee51e0a032a23e549f7
VioMrqs <violette.marquis@gmail.com>
2022-02-02 16:29:23 +0100

    filters finally working

--- src/components/BooksList/index.jsx
+++ src/components/BooksList/index.jsx
@@ -1,44 +1,67 @@
 import { useState } from "react";
 import Book from "../Book";
 
-const BooksList = ({ data }) => {  
+const BooksList = ({ data }) => {
   const [filterFav, setFilterFav] = useState(false);
-  const [filterWish, setFilterWish] = useState(false);
+  const [filterRead, setFilterRead] = useState(false);
 
   const handleSetFilterFav = () => {
     filterFav === true ? setFilterFav(false) : setFilterFav(true);
   };
 
-  const handleSetFilterWish = () => {
-    filterWish === true ? setFilterWish(false) : setFilterWish(true);
+  const handleSetFilterRead = () => {
+    filterRead === true ? setFilterRead(false) : setFilterRead(true);
   };
 
-  const booksList = data.books[0];
-
-  if(filterFav === true){
-    booksList.filter(e => e.isFav === true)
-  }
-
-  if (filterWish === true) {
-    booksList.filter((e) => e.read === true);
-  }
+  const defineBookList = () => {
+    let booksList = data.books[0];
+    if (filterFav === true && filterRead === true) {
+      return (booksList = booksList.filter(
+        (e) => e.isFav === true && e.read === true
+      ));
+    } else if (filterFav === true) {
+      return (booksList = booksList.filter((e) => e.isFav === true));
+    } else if (filterRead === true) {
+      return (booksList = booksList.filter((e) => e.read === true));
+    }
+    return booksList;
+  };
 
   return (
     <div className="Librairie">
       <button onClick={handleSetFilterFav}>
         {filterFav ? "Tous les livres" : "Mes favoris"}
       </button>
-      <button onClick={handleSetFilterWish}>
-        {filterWish ? "Tous les livres" : "Mes souhaits de lecture"}
+      <button onClick={handleSetFilterRead}>
+        {filterRead ? "Tous les livres" : "Mes souhaits"}
       </button>
       <div className="BookGrid">
-        {booksList.map((book) => (
-          <Book book={book}/>
+        {defineBookList().map((book) => (
+          <Book book={book} />
         ))}
         ;
       </div>
     </div>
   );
-};;
+};
 
 export default BooksList;
+
+// {
+//   () => {
+//     if (filterFav === true) {
+//       return booksList
+//         .filter((e) => e.isFav === true)
+//         .map((book) => <Book book={book} />);
+//     } else if (filterRead === true) {
+//       return booksList
+//         .filter((e) => e.read === true)
+//         .map((book) => <Book book={book} />);
+//     } else if (filterRead === true && filterFav === true) {
+//       return booksList
+//         .filter((e) => e.isFav === true && e.read === true)
+//         .map((book) => <Book book={book} />);
+//     }
+//     return booksList.map((book) => <Book book={book} />);
+//   };
+// }
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { useState } from "react";
 import BooksList from "./components/BooksList";
 import * as data from "./book.json";
 import './style.scss';
@@ -17,9 +16,3 @@ return (
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // Une barre de recherche est disponible, et les utilisateurs peuvent chercher un ou plusieurs livres en fonction de leur titre. Il y aura également 2 boutons, un permettant de filtrer les livres favoris, et un permettant de filtrer les livres que l'utilisateur souhaite lire.
-
-// Lorsque l'utilisateur clique sur le bouton "Filtrer selon mes favoris", le texte du bouton change pour "Retirer filtre favori" et seuls les livres favoris sont affichés. De même pour le bouton "Filtrer selon mes souhaits", le texte du bouton change pour "Retirer filtre souhait".
-
-// Si les deux filtres sont activés, l'utilisateur voit alors ses livres qu'il souhaite lire ET mis en favoris. C'est-à-dire non pas l'ensemble des livres souhaités et l'ensemble des livres mis en favoris, mais uniquement les livres qui respectent ces deux conditions.
-
-// Afin de comprendre comment mettre en place ces filtres, je t'invite à bien regarder les différents champs qui composent le JSON.
